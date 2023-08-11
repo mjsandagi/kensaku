281548a56b46d3273a53ca7eff89c5a9bcd39066
KazutoKiritoKirigaya <KazutoKiritoKirigaya@users.noreply.github.com>
2023-08-11 17:20:24 +0100

    Added search functionality

--- js/app.js
+++ js/app.js
@@ -18,11 +18,31 @@ async function getData(baseURL, searchTerm) {
     }
 }
 
+function showMoreInfo(malId, descrText) {
+    document.getElementById(
+        malId
+    ).innerHTML += `<p class="moreInfo">${descrText}</p>`;
+}
+
 searchElement.addEventListener("keydown", async (event) => {
     if (event.key == "Enter" && searchElement.value) {
         let uriSafeSearchTerm = encodeURIComponent(searchElement.value);
-        console.log(uriSafeSearchTerm);
+        // console.log(uriSafeSearchTerm);
         let searchData = await getData(baseURL, uriSafeSearchTerm);
         console.log(searchData.data);
+        resultsElement.innerHTML = "";
+        for (const [key, value] of Object.entries(searchData.data)) {
+            if (value.title_english) {
+                var titleElem = `<p><a href="${value.url}">${value.title} (${value.title_english})</a></p>`;
+            } else {
+                var titleElem = `<p><a href="${value.url}">${value.title}</a></p>`;
+            }
+            let imageElem = `<img class="animeImg" id="${value.mal_id}IMG" src="${value.images.jpg.image_url}" title="${value.title_japanese}">`;
+            let finalElem = `<div class="anime" id="${value.mal_id}">${titleElem}${imageElem}</div>`;
+            resultsElement.innerHTML += `${finalElem}`;
+            if (value.synopsis) {
+                showMoreInfo(value.mal_id, value.synopsis);
+            }
+        }
     }
 });
